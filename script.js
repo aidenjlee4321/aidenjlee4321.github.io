9fa3c90150cb87260357eea6784e5f74d39c9b2e
Aiden Lee <135405041+aidenjlee4321@users.noreply.github.com>
2026-03-14 03:05:21 +0000

    first files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+ 
+  const btn = document.getElementById("cta-btn");
+ 
+  btn.addEventListener("click", () => {
+    alert("Test");
+  });
+ 
+});
+ 
